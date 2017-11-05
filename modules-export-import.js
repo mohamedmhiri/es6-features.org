99d898d15cb5875eb4346f0abf21b947f014a638
mohamed mhiri <mohamed.mohamed.mhiri@gmail.com>
2017-11-05 12:03:01 +0100

    example#10

---
+++ modules-export-import.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import * as math from './modules'
+
+//  someApp.js 
+console.log("2π = " + math.sum(math.pi, math.pi))
+
+//  otherApp.js 
+import { sum, pi } from "./modules" 
+console.log("2π = " + sum(pi, pi))
