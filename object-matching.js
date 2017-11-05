08713ceb05ba778f0101aa45f1b3eba1405bb20a
mohamed mhiri <mohamed.mohamed.mhiri@gmail.com>
2017-11-05 11:39:14 +0100

    example#8

--- object-matching.js
+++ object-matching.js
@@ -5,4 +5,10 @@ function getASTNode () {
 
 var { op, lhs, rhs } = getASTNode()
 
-var { fr: a, lr: { ps: b }, tf: c } = getASTNode()
+//var { fr: a, lr: { ps: b }, tf: c } = getASTNode()
+
+var obj = { a: 1 } 
+var list = [ 1 ] 
+var { a, b = 2 } = obj 
+var [ x, y = 2 ] = list
+console.log(obj)
